2ccc5565749afb46d8b60f042608249f230d3ad2
JohanVonElectrum <joanestradium@gmail.com>
2026-04-04 05:28:56 +0200

    refactor: change call convention of `join_thread`

--- quark/src/platform/thread.h
+++ quark/src/platform/thread.h
@@ -8,7 +8,7 @@ typedef void*(*PFN_QuarkThreadFunc)(void* arg);
 QuarkThread* init_main_thread(void);
 
 QuarkThread* spawn_thread(PFN_QuarkThreadFunc func, void* arg);
-void* join_thread(QuarkThread* thread);
+QUARK_B8 join_thread(QuarkThread* thread, void** result);
 QUARK_U64 get_thread_id(const QuarkThread* thread);
 
 extern thread_local QuarkThread* current_thread;
--- quark/src/platform/thread_win32.c
+++ quark/src/platform/thread_win32.c
@@ -2,6 +2,8 @@
 
 #include "memory.h"
 
+#include <quark/core/assert.h>
+
 #include <process.h>
 #include <windows.h>
 
@@ -18,7 +20,12 @@ struct QuarkThread
 thread_local QuarkThread* current_thread;
 
 QuarkThread* init_main_thread(void) {
-    current_thread = (QuarkThread*)quark_mem_calloc(1, sizeof(QuarkThread));
+    QUARK_ASSERT_RETURN(
+        current_thread->is_main_thread ? current_thread : nullptr,
+        current_thread == nullptr,
+        "Main thread already initialized"
+    );
+    current_thread = (QuarkThread*) quark_mem_calloc(1, sizeof(QuarkThread));
     if (!current_thread) return nullptr;
 
     current_thread->handle = GetCurrentThread();
@@ -29,7 +36,7 @@ QuarkThread* init_main_thread(void) {
 }
 
 static unsigned __stdcall thread_wrapper(void* arg) {
-    QuarkThread* thread = (QuarkThread*)arg;
+    QuarkThread* thread = (QuarkThread*) arg;
     current_thread = thread;
     thread->ret = thread->func(thread->arg);
     return 0;
@@ -43,23 +50,34 @@ QuarkThread* spawn_thread(const PFN_QuarkThreadFunc func, void* arg) {
     thread->arg = arg;
     thread->is_main_thread = QUARK_FALSE;
 
-    thread->handle = (HANDLE)_beginthreadex(nullptr, 0, thread_wrapper, thread, 0, &thread->tid);
-    if (thread->handle == nullptr) {
+    thread->handle = (HANDLE) _beginthreadex(nullptr, 0, thread_wrapper, thread, 0, &thread->tid);
+    QUARK_ASSERT_X(
+        {
         quark_mem_free(thread);
         return nullptr;
-    }
+        },
+        thread->handle != nullptr,
+        "Failed to create thread"
+    );
     return thread;
 }
 
-void* join_thread(QuarkThread* thread) {
-    if (!thread) return nullptr;
-    if (thread->is_main_thread) return nullptr;
+QUARK_B8 join_thread(QuarkThread* thread, void** result) {
+    QUARK_ASSERT_RETURN(
+        QUARK_FALSE,
+        thread != nullptr,
+        "Thread is null"
+    );
+    QUARK_ASSERT_RETURN(
+        QUARK_FALSE,
+        !thread->is_main_thread,
+        "Cannot join main thread"
+    )
 
     WaitForSingleObject(thread->handle, INFINITE);
     CloseHandle(thread->handle);
-    void* ret = thread->ret;
-    quark_mem_free(thread);
-    return ret;
+    if (result) *result = thread->ret;
+    return quark_mem_free(thread);
 }
 
 QUARK_U64 get_thread_id(const QuarkThread* thread) {
